add8ba41747e228553e7380671347dcd4b1a5117
Gaku52 <Gaku52@example.com>
2022-09-20 03:29:56 +0900

    ポートフォリオ ver.004

---
+++ src/components/BlogPage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className="container">
+      <h1>ブログ</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis architecto distinctio quia perspiciatis soluta blanditiis, molestiae ducimus quos ipsum. Accusantium sit consectetur nemo at inventore ut temporibus unde tempora repudiandae beatae ratione, suscipit neque possimus? Ad animi inventore itaque sed, quae quidem nesciunt nobis, aut voluptate esse impedit nostrum necessitatibus voluptas culpa neque odit repellat corporis, ea quis perferendis doloribus? Maiores commodi delectus assumenda libero architecto, praesentium fugit iusto id unde nulla ea ex. Ullam beatae numquam minima consectetur, quia vel eum. Temporibus illum commodi tempore soluta laborum quas provident nemo nobis, magnam eligendi nihil? Saepe ratione perspiciatis eveniet consequuntur?
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis architecto distinctio quia perspiciatis soluta blanditiis, molestiae ducimus quos ipsum. Accusantium sit consectetur nemo at inventore ut temporibus unde tempora repudiandae beatae ratione, suscipit neque possimus? Ad animi inventore itaque sed, quae quidem nesciunt nobis, aut voluptate esse impedit nostrum necessitatibus voluptas culpa neque odit repellat corporis, ea quis perferendis doloribus? Maiores commodi delectus assumenda libero architecto, praesentium fugit iusto id unde nulla ea ex. Ullam beatae numquam minima consectetur, quia vel eum. Temporibus illum commodi tempore soluta laborum quas provident nemo nobis, magnam eligendi nihil? Saepe ratione perspiciatis eveniet consequuntur?
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis architecto distinctio quia perspiciatis soluta blanditiis, molestiae ducimus quos ipsum. Accusantium sit consectetur nemo at inventore ut temporibus unde tempora repudiandae beatae ratione, suscipit neque possimus? Ad animi inventore itaque sed, quae quidem nesciunt nobis, aut voluptate esse impedit nostrum necessitatibus voluptas culpa neque odit repellat corporis, ea quis perferendis doloribus? Maiores commodi delectus assumenda libero architecto, praesentium fugit iusto id unde nulla ea ex. Ullam beatae numquam minima consectetur, quia vel eum. Temporibus illum commodi tempore soluta laborum quas provident nemo nobis, magnam eligendi nihil? Saepe ratione perspiciatis eveniet consequuntur?
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis architecto distinctio quia perspiciatis soluta blanditiis, molestiae ducimus quos ipsum. Accusantium sit consectetur nemo at inventore ut temporibus unde tempora repudiandae beatae ratione, suscipit neque possimus? Ad animi inventore itaque sed, quae quidem nesciunt nobis, aut voluptate esse impedit nostrum necessitatibus voluptas culpa neque odit repellat corporis, ea quis perferendis doloribus? Maiores commodi delectus assumenda libero architecto, praesentium fugit iusto id unde nulla ea ex. Ullam beatae numquam minima consectetur, quia vel eum. Temporibus illum commodi tempore soluta laborum quas provident nemo nobis, magnam eligendi nihil? Saepe ratione perspiciatis eveniet consequuntur?
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis architecto distinctio quia perspiciatis soluta blanditiis, molestiae ducimus quos ipsum. Accusantium sit consectetur nemo at inventore ut temporibus unde tempora repudiandae beatae ratione, suscipit neque possimus? Ad animi inventore itaque sed, quae quidem nesciunt nobis, aut voluptate esse impedit nostrum necessitatibus voluptas culpa neque odit repellat corporis, ea quis perferendis doloribus? Maiores commodi delectus assumenda libero architecto, praesentium fugit iusto id unde nulla ea ex. Ullam beatae numquam minima consectetur, quia vel eum. Temporibus illum commodi tempore soluta laborum quas provident nemo nobis, magnam eligendi nihil? Saepe ratione perspiciatis eveniet consequuntur?
+      </p>
+    </div>
+  )
+}
+
+export default BlogPage
---
+++ src/components/SnsPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SnsPage = () => {
+  return (
+    <div className="container">
+      <h1>SNS</h1>
+      <p>Twitter</p>
+    </div>
+  )
+}
+
+export default SnsPage
